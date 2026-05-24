ae145c44f8976ef8b75dc2d8df2e3d5f46a56da6
ammadwahga0 <ammadwahga0@gmail.com>
2026-05-24 14:15:14 +0500

    fix route import casing

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const authRoutes = require("./routes/authRoutes");
+const authRoutes = require("./routes/authroutes");
 const productRoutes = require("./routes/productroutes");
 const orderRoutes = require("./routes/orderroutes");
 const analyticsRoutes = require("./routes/analyticsroutes");
